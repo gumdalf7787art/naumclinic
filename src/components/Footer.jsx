948f7c5d606049cb85e0226f07129ab1be82b27a
Gumdalf <gumdalf7787art@gmail.com>
2026-09-09 23:28:56 +0900

    fix: Footer.jsx 누락된 useNavigate 컴포넌트 import 오류 수정

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useCMSData } from '../hooks/useCMS';
 import * as C from '../constants/hospitalData';
 function Footer() {
